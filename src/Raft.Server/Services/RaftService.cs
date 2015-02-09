1fe321a8e368a2343e7c7151aeff166441b42c48
yburke <yussuf.burke@dfconline.com>
2015-02-09 16:33:26 +0000

    Started on RPC buffers

--- src/Raft.Server/Services/RaftService.cs
+++ src/Raft.Server/Services/RaftService.cs
@@ -1,4 +1,5 @@
-﻿using Raft.Core;
+﻿using Disruptor;
+using Raft.Core;
 using Raft.Server.Messages.AppendEntries;
 using Raft.Server.Messages.RequestVote;
 
@@ -6,11 +7,18 @@ namespace Raft.Server.Services
 {
     internal class RaftService : IRaftService
     {
+        private readonly EventPublisher<CommitRequestedEvent> _commitPublisher;
+        private readonly EventPublisher<ApplyRequestedEvent> _applyPublisher;
         private readonly INodeTimer _timer;
         private readonly IRaftNode _raftNode;
 
-        public RaftService(INodeTimer timer, IRaftNode raftNode)
+        public RaftService(EventPublisher<CommitRequestedEvent> commitPublisher,
+            EventPublisher<ApplyRequestedEvent> applyPublisher,
+            INodeTimer timer, IRaftNode raftNode)
         {
+            _commitPublisher = commitPublisher;
+            _applyPublisher = applyPublisher;
+
             _timer = timer;
             _raftNode = raftNode;
         }
@@ -45,5 +53,25 @@ namespace Raft.Server.Services
                 Term = _raftNode.CurrentTerm
             };
         }
+    }
+
+    internal class ApplyRequestedEvent
+    {
+        public long LogIdx { get; set; }
+
+        public ApplyRequestedEvent ResetEvent(long logIdx)
+        {
+            // Finish impl... Refactor these out of events!!!
+            return null;
+        }
+    }
+
+    internal class CommitRequestedEvent
+    {
+        public long Term { get; set; }
+
+        public long LogIdx { get; set; }
+
+        public byte[] Entry { get; set; }
     }
 }
