bc6c9202b56789779ccc37d0615175b7176d80b3
yburke <yussuf.burke@dfconline.com>
2015-04-13 20:17:28 +0100

    Forgot to commit a file

--- src/Raft/Server/Handlers/Follower/RpcLogWriter.cs
+++ src/Raft/Server/Handlers/Follower/RpcLogWriter.cs
@@ -1,10 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Disruptor;
 using ProtoBuf;
 using Raft.Contracts.Persistance;
 using Raft.Core.Commands;
+using Raft.Core.StateMachine;
 using Raft.Infrastructure.Disruptor;
-using Raft.Infrastructure.Journaler;
 using Raft.Server.BufferEvents;
 using Raft.Server.Data;
 
@@ -13,49 +16,67 @@ namespace Raft.Server.Handlers.Follower
     internal class RpcLogWriter : IEventHandler<AppendEntriesRequested>
     {
         private readonly IWriteDataBlocks _writeDataBlocks;
-        private readonly CommandRegister _commandRegister;
+        private readonly INode _node;
         private readonly IPublishToBuffer<NodeCommandScheduled, NodeCommandResult> _nodePublisher;
 
-        public RpcLogWriter(IWriteDataBlocks writeDataBlocks, CommandRegister commandRegister,
+        public RpcLogWriter(IWriteDataBlocks writeDataBlocks, INode node,
             IPublishToBuffer<NodeCommandScheduled, NodeCommandResult> nodePublisher)
         {
             _writeDataBlocks = writeDataBlocks;
-            _commandRegister = commandRegister;
+            _node = node;
             _nodePublisher = nodePublisher;
         }
 
         public void OnNext(AppendEntriesRequested data, long sequence, bool endOfBatch)
         {
-            try
-            {
-                // TODO: This needs to take into account fixing the log in the event of log matching.
-                // TODO: As the commands are decoded, it should apply them if their index is <= CommitIndex.
-                // TODO: Both the log Commit and Apply logic can and should be extracted and shared with the leader buffer.
-                // TODO: Test the crap out of this...
-                // LogEntry decodedEntry;
-                // using (var stream = new MemoryStream(data.Entry, false))
-                // {
-                //     decodedEntry = Serializer.DeserializeWithLengthPrefix<LogEntry>(stream, PrefixStyle.Base128);
-                // }
-                // 
-                // // TODO: Generate checksum and compare?.
-                // _writeDataBlocks.WriteBlock(data.Entry);
-                // _nodePublisher.PublishEvent(new NodeCommandScheduled
-                // {
-                //     Command = new CommitEntry
-                //     {
-                //         EntryIdx = decodedEntry.Index,
-                //         EntryTerm = decodedEntry.Term
-                //     }
-                // }).Wait();
-                // 
-                // _commandRegister.Add(decodedEntry.Term, decodedEntry.Index, decodedEntry.Command);
-            }
-            catch
-            {
-                //  TODO: Add logging and log error
-                // _log.Error("Failed to apply log: {logIndex} for term: {term}")
-            }
+            if (data.Entries == null || !data.Entries.Any()) return;
+
+            var entries = data.Entries
+                .Select(entry =>
+                {
+                    using (var ms = new MemoryStream(entry))
+                    {
+                        var logEntry = Serializer.DeserializeWithLengthPrefix<LogEntry>(ms, PrefixStyle.Base128);
+                        return new
+                        {
+                            Entry = entry,
+                            DeserializedEntry = logEntry
+                        };
+                    }
+                })
+                .OrderBy(x => x.DeserializedEntry.Index)
+                .ToList();
+            
+            var lowestIdx = entries.First().DeserializedEntry.Index;
+            if (lowestIdx != _node.Data.CommitIndex+1)
+                throw new InvalidOperationException(string.Format(
+                    "The request sent was invalid. The current commit index for the node is '{0}'. " +
+                    "The lowest log entry index was expected to be '{1}' but was '{2}'.",
+                    _node.Data.CommitIndex, _node.Data.CommitIndex+1, lowestIdx));
+
+            data.EntriesDeserialized = entries.Select(x => x.DeserializedEntry).ToArray();
+
+            var dataBlocks = entries
+                .Select(x => new DataBlock
+                {
+                    Data = x.Entry,
+                    Metadata = new Dictionary<string, string>
+                    {
+                        {"BodyType", typeof (LogEntry).AssemblyQualifiedName}
+                    }
+                })
+                .ToArray();
+
+            _writeDataBlocks.WriteBlocks(dataBlocks);
+            entries.ForEach(entry => _nodePublisher.PublishEvent(
+                new NodeCommandScheduled
+                {
+                    Command = new CommitEntry
+                    {
+                        EntryIdx = entry.DeserializedEntry.Index,
+                        EntryTerm = entry.DeserializedEntry.Term
+                    }
+                }));
         }
     }
 }
