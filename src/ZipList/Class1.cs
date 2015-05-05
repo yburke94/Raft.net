2cd5e0460ab48093d949bdb55b135c9145169a1a
yburke <yussuf.burke@dfconline.com>
2015-05-05 16:49:04 +0100

    Ziplist Push

--- src/ZipList/Class1.cs
+++ src/ZipList/Class1.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZipList
 {
@@ -31,16 +30,33 @@ namespace ZipList
  * able to traverse the list from back to front. Second, the encoding with an
  * optional string length of the entry itself is stored.
          
-         *Length
+         *Length (Entries)
+         *Size (Blob Length)
          * 
          * *Pop
          * 
-         * *Push
-         * *PushAll
+         * Push
+         * PushAll
+         * Truncate
+         * Iterate
+         */
+
+        /*
+         Orig:
          * 
-         * *Truncate
+         * HEAD | Tail
          * 
-         * *Iterate
+         * New
+         * Merge
+         * Push
+         * Index??
+         * Next/PRev {Iterator}
+         * Get(len)
+         * Insert(len)
+         * Delete
+         * DeleteRange
+         * Compare
+         * Find
          */
     }
 
@@ -49,27 +65,52 @@ namespace ZipList
     /// </summary>
     public class ZipList
     {
-        private ulong _bytes;
-        private ulong _tail;
-        private ulong _length;
+        private long _bytes;
+        private long _tail;
+        private long _length;
+
+        private const int SizeOfHeaderVariables = sizeof(long);
 
         private const byte Eol = 0xFF; // End of list
+        private const int SizeOfEol = sizeof(byte);
+
         private const ushort MaxIncrement = ushort.MaxValue; // Why... I Dont know why...
 
-        const int BytesOffset = 2 << 0;
-        const int TailOffset = 2 << (sizeof(ulong)*8);
-        const int LengthOffset = 2 << ((sizeof(ulong) * 8) * 2);
+        const int BytesOffset = 0;
+        const int TailOffset = sizeof(long);
+        const int LengthOffset = sizeof(long) *2;
 
-        private byte[] _list;
+        private byte[] _blob;
 
-        public ulong Length
+        /// <summary>
+        /// No of entries.
+        /// </summary>
+        public long Length
         {
             get { return _length; }
         }
 
+        /// <summary>
+        /// Size of list in bytes.
+        /// </summary>
+        public long SizeOfList
+        {
+            get { return _bytes; }
+        }
+
+        /// <summary>
+        /// Size of blob in bytes.
+        /// </summary>
+        public long SizeInMemory
+        {
+            get { return _blob.LongLength; }
+        }
+
+
+
         public ZipList()
         {
-            _list = new byte[MaxIncrement];
+            _blob = new byte[MaxIncrement];
 
             _bytes = 0L;
             _tail = 0L;
@@ -80,24 +121,21 @@ namespace ZipList
 
         private void Init()
         {
-            if (_length != 0) throw new Exception(); // TODO: errm!!!
+            _bytes += (SizeOfHeaderVariables*3) + SizeOfEol;
+            ExtendBlockIfRequired(ref _blob, _bytes, 0L);
 
-            const int bytesToAdd = (sizeof (long)*3) + sizeof (byte); // <bytes><tail><length><eol>
-            ExtendBlockIfRequired(ref _list, bytesToAdd, BytesOffset);
+            WriteHeaderVariable(_blob, BytesOffset, _bytes);
+            WriteHeaderVariable(_blob, TailOffset, _tail);
+            WriteHeaderVariable(_blob, LengthOffset, _length);
 
-            Array.Copy(BitConverter.GetBytes(_bytes), 0, _list, BytesOffset, sizeof(long));
-            Array.Copy(BitConverter.GetBytes(_tail), 0, _list, TailOffset, sizeof(long));
-            Array.Copy(BitConverter.GetBytes(_length), 0, _list, LengthOffset, sizeof(long));
-            Array.Copy(new []{Eol}, 0, _list, LengthOffset+sizeof(long), 1);
-
-            _bytes += bytesToAdd;
-
-            RewriteVariable(_list, BytesOffset, _bytes);
+            WriteEol(_blob, LengthOffset+SizeOfHeaderVariables);
         }
 
         public byte[] GetBytes()
         {
-            return _list;
+            var ret = new byte[_bytes];
+            Array.Copy(_blob, ret, _bytes);
+            return ret;
         }
 
         public static ZipList FromBytes(byte[] bytes)
@@ -106,27 +144,50 @@ namespace ZipList
 
         public bool HasEntries
         {
-            get { return _length > 0 && _tail > 0; }
+            get { return _length > 0; }
         }
 
-        public byte[] Pop()
+        public byte[] Get(long idx)
         {
 
         }
 
         public void Push(byte[] bytes)
         {
-            var lengthAdded = bytes.Length;
-            ExtendBlockIfRequired(ref _list, (ulong)lengthAdded, _bytes);
-
-            _length++;
-
-            // _tail /*if 0 = offsets + length else += length*/
+            PushAll(new []{bytes});
         }
 
         public void PushAll(byte[][] byteBlocks)
         {
+            var totalBytes = (byteBlocks.Length*(SizeOfHeaderVariables*2)) + byteBlocks.Sum(x => x.LongLength);
+            ExtendBlockIfRequired(ref _blob, totalBytes, _bytes);
 
+            foreach (var bytes in byteBlocks)
+            {
+                // Write List Header
+                var oldTail = _tail;
+                _tail = _bytes - SizeOfEol;
+
+                var sizeOfEntry = (SizeOfHeaderVariables * 2) + bytes.Length;
+                _bytes += sizeOfEntry;
+
+                WriteHeaderVariable(_blob, BytesOffset, _bytes);
+
+                WriteHeaderVariable(_blob, TailOffset, _tail);
+
+                _length++;
+                WriteHeaderVariable(_blob, LengthOffset, _length);
+
+                // Write Entry
+                WriteHeaderVariable(_blob, _tail, oldTail); // PrevLength
+                WriteHeaderVariable(_blob, _tail + SizeOfHeaderVariables, bytes.LongLength); // DataLength
+
+                var entryStart = _tail + (SizeOfHeaderVariables * 2);
+                Write(_blob, entryStart, bytes); // Entry
+            }
+
+            // Write Eol
+            WriteEol(_blob, _bytes - SizeOfEol);
         }
 
         public void Merge(ZipList zipList)
@@ -142,19 +203,36 @@ namespace ZipList
             
         }
 
-        private static void RewriteVariable(byte[] block, int offset, ulong value)
+        private static void WriteHeaderVariable(byte[] block, long offset, long value)
         {
-            var valueAsBits = BitConverter.GetBytes(value);
-
-            for (var i = 0; i < sizeof(long); i++)
-                block[offset + i] = valueAsBits[i];
+            var valueAsBytes = BitConverter.GetBytes(value);
+            Write(block, offset, valueAsBytes);
         }
 
-
-        private static void ExtendBlockIfRequired(ref byte[] block, ulong lengthModified, ulong offset)
+        private static void WriteEol(byte[] block, long offset)
         {
-            var maxChangeLength = offset + lengthModified;
-            if (maxChangeLength < (ulong)block.Length) return;
+            Write(block, offset, new []{Eol});
+        }
+
+        private static void Write(byte[] block, long offset, byte[] value)
+        {
+            Array.Copy(value, 0, block, offset, value.Length);
+        }
+
+        private static long ReadHeaderVariable(byte[] block, long offset)
+        {
+            var ret = new byte[SizeOfHeaderVariables];
+
+            for (var i = 0; i < SizeOfHeaderVariables; i++)
+                ret[i] = block[offset + i];
+
+            return BitConverter.ToInt64(ret, 0);
+        }
+
+        private static void ExtendBlockIfRequired(ref byte[] block, long lengthAdded, long offset)
+        {
+            var maxChangeLength = offset + lengthAdded;
+            if (maxChangeLength < block.LongLength) return;
 
             var newBlock = new byte[block.Length + MaxIncrement];
             Array.Copy(block, newBlock, block.Length);
